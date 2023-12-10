ef2844f02ef50328f461108fdf830586fb8a2b31
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2023-12-11 02:14:07 +0300

    prod 2

--- src/gem/search.py
+++ src/gem/search.py
@@ -32,6 +32,8 @@ def init_browser() -> webdriver.Chrome:
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument(f"user-agent={UserAgent.random}")
     s = Service()
     driver = webdriver.Chrome(
