191052dbd5efece7419e33fc725e67275f9f111d
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2023-12-11 01:09:32 +0300

    prod 2

--- src/gem/search.py
+++ src/gem/search.py
@@ -29,10 +29,15 @@ search_results_list_x_path = "/html/body/div[4]/div[1]/div/div/div[1]/div/div/di
 
 def init_browser() -> webdriver.Chrome:
     options = webdriver.ChromeOptions()
-    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument("--window-size=1920,1080")
+
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3"
-    options.add_argument(f"user-agent={user_agent}")
+    options.add_argument(f'user-agent={user_agent}')
     browser = webdriver.Chrome(
         options=options
     )
