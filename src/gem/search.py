1176905e062f422c8901bd88da1d34d47c9e25cc
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2023-12-11 03:15:59 +0300

    prod 2

--- src/gem/search.py
+++ src/gem/search.py
@@ -34,6 +34,17 @@ def init_browser() -> webdriver.Chrome:
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("--headless")
     options.add_argument('--no-sandbox')
+
+    options.add_argument('--no-sandbox')
+    options.add_argument('start-maximized')
+    options.add_argument('enable-automation')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-browser-side-navigation')
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument('--disable-gpu')
+    options.add_argument("--log-level=3")
+
     options.add_argument("--window-size=1920,1080")
 
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
@@ -55,6 +66,8 @@ def init_browser() -> webdriver.Chrome:
         }
     )
     print(res)
+    browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
     browser.get("https://bot.sannysoft.com/")
     browser.save_screenshot("antibot_screenshot.png")
     return browser
