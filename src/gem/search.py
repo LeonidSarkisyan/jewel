282ba55830b17329c8e779bd00693c0ce8bb3336
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2023-12-11 13:43:37 +0300

    prod 3

--- src/gem/search.py
+++ src/gem/search.py
@@ -29,27 +29,13 @@ search_results_list_x_path = "/html/body/div[4]/div[1]/div/div/div[1]/div/div/di
 
 def init_browser() -> webdriver.Chrome:
     options = webdriver.ChromeOptions()
-    options.add_argument("start-maximized")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument("--headless")
-    options.add_argument('--no-sandbox')
 
-    options.add_argument('--no-sandbox')
-    options.add_argument('start-maximized')
-    options.add_argument('enable-automation')
-    options.add_argument('--disable-infobars')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-browser-side-navigation')
-    options.add_argument("--remote-debugging-port=9222")
-    options.add_argument('--disable-gpu')
-    options.add_argument("--log-level=3")
     options.add_argument("--disable-blink-features=AutomationControlled")
-
-    options.add_argument("--window-size=1920,1080")
-
+    options.add_argument('start-maximized')
+    options.add_argument("--headless")
     user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2'
     options.add_argument(f'user-agent={user_agent}')
+
     browser = webdriver.Chrome(
         options=options
     )
